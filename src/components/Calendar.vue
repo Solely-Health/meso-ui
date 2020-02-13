146ec98bc563c432e6c142fb23e0ad2b9823523d
alecnicolas <alecnmarcum@gmail.com>
2020-02-13 14:57:32 -0800

    laying out the boi

---
+++ src/components/Calendar.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-calendar></v-calendar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Calendar',
+
+  data: () => ({
+  }),
+});
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <Calendar />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Calendar from '../components/Calendar.vue';
+
+export default Vue.extend({
+  name: 'Home',
+
+  components: {
+    Calendar,
+  },
+});
+</script>
