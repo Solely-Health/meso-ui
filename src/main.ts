a7486b37730ab3a16f6669fafee4871044d4d052
alecnicolas <alecnmarcum@gmail.com>
2020-02-13 16:45:23 -0800

    typescript configuring

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
+// import Vuetify from 'vuetify';
+// import 'vuetify/dist/vuetify.min.css';
+// Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
@@ -10,5 +13,5 @@ new Vue({
   router,
   store,
   vuetify,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount('#app');
