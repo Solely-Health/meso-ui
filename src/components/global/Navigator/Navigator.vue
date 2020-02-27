fe620530354cd78c5386bcebb7750ce29019212a
ekno27 <edgkano@gmail.com>
2020-02-27 13:54:44 -0800

    chevrons

--- src/components/global/Navigator/Navigator.vue
+++ src/components/global/Navigator/Navigator.vue
@@ -1,9 +1,16 @@
 
 <template>
 <div class="">
-        <v-btn style="width: 8rem;" class="text-center mr-10" large > Previous</v-btn>
+        <v-btn style="width: 8rem;" class="text-center mr-10" large >
+          <i class="fas fa-chevron-left"></i>
+            Previous
+          </v-btn>
 
-        <v-btn style="width: 8rem;" class="text-center" large color="primary">Next</v-btn>
+        <v-btn style="width: 8rem;" class="text-center" large color="primary">
+          Next  
+          <i class="fas fa-chevron-right"></i>     
+
+        </v-btn>
       </div>
 </template>
 
