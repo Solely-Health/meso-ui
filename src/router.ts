e5d265c6237607dd8ed9636239aa58233cf289f9
Edgar A. Cano <32141100+ekno27@users.noreply.github.com>
2020-03-03 10:41:51 -0800

    IN-50: Skills, Search Radius and Completed screen components
    * skills, search, zipcode, locate me, complete screen

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-
+import Skills from './views/skills/Skills.vue';
+import Location from './views/location/Location.vue';
+import RegistrationComplete from './views/registration_complete/Registration_Complete.vue';
 Vue.use(Router);
 
 export default new Router({
@@ -12,12 +14,27 @@ export default new Router({
       component: Home,
     },
     {
+      path: '/skills',
+      name: 'skills',
+      component: Skills,
+    },
+    {
+      path: '/complete',
+      name: 'complete',
+      component: RegistrationComplete,
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+    {
+      path: '/location',
+      name: 'location',
+      component: Location,
     },
   ],
 });
