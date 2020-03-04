267e9ef1123cadb0421aaf663f25655550bbfec8
alecnicolas <alecnmarcum@gmail.com>
2020-03-03 16:48:00 -0800

    formatting cuz lol

--- src/router.ts
+++ src/router.ts
@@ -9,39 +9,39 @@ import RegistrationComplete from './views/registration_complete/Registration_Com
 Vue.use( Router );
 
 export default new Router( {
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home,
-        },
-        {
-            path: '/skills',
-            name: 'skills',
-            component: Skills,
-        },
-        {
-            path: '/complete',
-            name: 'complete',
-            component: RegistrationComplete,
-        },
-        {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ './views/About.vue' ),
-        },
-        {
-            path: '/dashboard',
-            name: 'dashboard',
-            component: Dashboard,
-        },
-        {
-            path: '/location',
-            name: 'location',
-            component: Location,
-        },
-    ],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/skills',
+      name: 'skills',
+      component: Skills,
+    },
+    {
+      path: '/complete',
+      name: 'complete',
+      component: RegistrationComplete,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue' ),
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+    },
+    {
+      path: '/location',
+      name: 'location',
+      component: Location,
+    },
+  ],
 } );
