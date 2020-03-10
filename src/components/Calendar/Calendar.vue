dd69e2d3e126c1ca418561cb2cab8fdcc4bf1c1b
ekno27 <edgkano@gmail.com>
2020-03-10 10:09:43 -0700

    started working on navbar

--- src/components/Calendar/Calendar.vue
+++ src/components/Calendar/Calendar.vue
@@ -1,19 +1,44 @@
 <template>
-    <v-calendar
-      ref="calendar"
-      v-model="start"
-      :type="type"
-      :start="start"
-      :end="end"
-      :weekdays="weekdays"
-      :short-months="true"
-      :short-weekdays="true"
-      :color="color"
-      :events="events"
-      :event-color="getEventColor"
-      :event-overlap-mode="mode"
-      :event-overlap-threshold="45"
-    ></v-calendar>
+    <div>
+      <v-app-bar
+        color="deep-purple accent-4"
+        dark
+        prominent
+      >
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+  
+        <v-toolbar-title>My files</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-filter</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <v-calendar
+        ref="calendar"
+        v-model="start"
+        :type="type"
+        :start="start"
+        :end="end"
+        :weekdays="weekdays"
+        :short-months="true"
+        :short-weekdays="true"
+        :color="color"
+        :events="events"
+        :event-color="getEventColor"
+        :event-overlap-mode="mode"
+        :event-overlap-threshold="45"
+      ></v-calendar>
+    </div>
 </template>
 
 <script lang="ts">
