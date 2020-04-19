ff648600602a961c770e9ea343c043b4a557e0cd
Mikkal <mcnultymikkal@gmail.com>
2020-04-19 15:29:56 -0700

    Add temporary cards for certs and skills

--- src/views/position/Position.vue
+++ src/views/position/Position.vue
@@ -1,11 +1,8 @@
 <template>
   <v-container>
+    <br />
     <v-card class="mx-auto" max-width="400">
-      <v-img
-        class="white--text align-end"
-        height="200px"
-        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-      ></v-img>
+      <v-img class="white--text align-end" height="200px" src="./static-facility.png"></v-img>
       <v-card-title>Respiratory Therapist</v-card-title>
       <v-card-subtitle class="pb-0">
         <a>West Hills Hospital</a>
@@ -24,18 +21,43 @@
         <v-btn color="orange" text>Interested</v-btn>
       </v-card-actions>
     </v-card>
-    <br/>
+    <br />
     <v-card class="mx-auto" max-width="400">
       <v-card-text>
         <p class="display-1 text--primary">Description</p>
-        <div class="text--primary">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nesciunt placeat, architecto ipsum vitae temporibus eveniet eligendi sit consequatur laudantium dolor. Veniam architecto quos quod quidem rem quae, repellat in..
-        </div>
+        <div
+          class="text--primary"
+        >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem nesciunt placeat, architecto ipsum vitae temporibus eveniet eligendi sit consequatur laudantium dolor. Veniam architecto quos quod quidem rem quae, repellat in..</div>
       </v-card-text>
       <v-card-actions>
         <v-btn text color="deep-purple accent-4">Learn More</v-btn>
       </v-card-actions>
     </v-card>
+
+    <br />
+    <v-card class="mx-auto" max-width="400">
+      <v-card-text>
+        <p class="display-1 text--primary">Certificates</p>
+        <div>
+            <p>Certifcate 1</p>
+            <p>Certifcate 3</p>
+            <p>Certifcate 2</p>
+        </div>
+      </v-card-text>
+    </v-card>
+
+    <br />
+    <v-card class="mx-auto" max-width="400">
+      <v-card-text>
+        <p class="display-1 text--primary">Skills</p>
+        <div>
+            <p>Skill 1</p>
+            <p>Skill 3</p>
+            <p>Skill 2</p>
+        </div>
+      </v-card-text>
+    </v-card>
+
   </v-container>
 </template>
 
