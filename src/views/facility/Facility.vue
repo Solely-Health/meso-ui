9db38c996e3fd8600f87ac677ecf81c87f802681
jazperez <jackkiey24@gmail.com>
2020-04-20 16:51:13 -0700

    Add details to Facility view

--- src/views/facility/Facility.vue
+++ src/views/facility/Facility.vue
@@ -4,19 +4,31 @@
     <v-layout align-center justify-center>
       <v-card class="ma-5">
         <v-card-title>Description</v-card-title>
-        <v-card-subtitle></v-card-subtitle>
+        <v-card-subtitle>{{description}}</v-card-subtitle>
       </v-card>
     </v-layout>
     <v-layout align-center justify-center>
       <v-card class="ma-5">
         <v-card-title>Certificates</v-card-title>
-        <v-card-subtitle></v-card-subtitle>
+        <v-flex wrap>
+          <v-chip close v-for="license in licenses" 
+            :key="license" 
+            class="chips pa-3 ma-2"
+            color="#75b78c"
+            text-color="white">{{license}}</v-chip>
+        </v-flex>
       </v-card>
     </v-layout>
     <v-layout align-center justify-center>
       <v-card class="ma-5">
         <v-card-title>Skills</v-card-title>
-        <v-card-subtitle></v-card-subtitle>
+        <v-flex wrap>
+          <v-chip close v-for="skill in skills" 
+            :key="skill"
+            class="chips pa-3 ma-2"
+            color="#75b78c"
+            text-color="white">{{skill}}</v-chip>
+        </v-flex>
       </v-card>
     </v-layout>
 
@@ -28,7 +40,7 @@ import Vue from 'vue';
 import FacilityCard from './../../components/FacilityCard/FacilityCard.vue';
 export default Vue.extend({
   data: () => ({
-        description: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'],
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         licenses: ['CPR' , 'Respritory Therapist', 'Registered Nurse'],
         skills: ['Patient monitoring systems' , 'Vital sign monitoring', 'Ethical', 'Diagnostic Equipment', 'Strict Attention To Detail'],
 }),
